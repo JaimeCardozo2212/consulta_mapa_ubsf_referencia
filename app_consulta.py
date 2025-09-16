3a6fbc13ac2169996351ac27c9bb144dd3f88303
jaimecardozo2212 <jaimecardozo.junior@gmail.com>
2025-09-16 11:51:51 -0300

    teste de commit

--- app_consulta.py
+++ app_consulta.py
@@ -102,7 +102,7 @@ def buscar_unidade(navegador, endereco):
 @st.cache_resource
 def get_driver():
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
